3d902374a9c19cdf8458f96a5e2acd5d983fd95c
NunoPalma <nunopalma98@gmail.com>
2019-09-13 13:33:00 +0100

    Bridge

--- Bridge/Person.java
+++ Bridge/Person.java
@@ -1,12 +1,18 @@
 abstract class Person {
 	private String _name;
+	private Task _task;
 
-	public Person(String name) {
+	public Person(String name, Task task) {
 		_name = name;
+		_task = task;
 	}
 
 	public String getName() {
 		return _name;
+	}
+
+	public Task getTask() {
+		return _task;
 	}
 
 	abstract public void doTask();
--- Bridge/Student.java
+++ Bridge/Student.java
@@ -1,13 +1,11 @@
 public class Student extends Person {
-	private Task _task;
 
 	public Student(String name, Task task) {
-		super(name);
-		_task = task;
+		super(name, task);
 	}
 
 	public void doTask() {
 		System.out.print(this.getName() +  " working on ");
-		 _task.doTask();
+		 this.getTask().doTask();
 	}
 }
